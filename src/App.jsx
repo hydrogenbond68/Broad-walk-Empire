7a93c6b6da3cde4c42e8be4e72eb574f49b0ca01
Bond <hydrogenbond68@gmail.com>
2024-05-09 07:35:39 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-
-
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -14,7 +12,45 @@ import SearchBar from "./components/SearchBar";
 import LogIn from "./components/Users";
 import Navbar from "./components/NavBar";
 import About from "./components/About";
-import Users from "./components/Users"
+import Users from "./components/Users";
+import Property from"./components/Property";
+
+export const PropertyDetails = ({ property }) => {
+  return (
+    <div className="property-details-page">
+      <h1 className="property-title">{property.title}</h1>
+      <div className="property-images">
+        {property.images.map((image, index) => (
+          <img key={index} src={image} alt={`Property ${index + 1}`} />
+        ))}
+      </div>
+      <div className="property-info">
+        <h3>Description</h3>
+        <p>{property.description}</p>
+        <h3>Features</h3>
+        <ul>
+          {property.features.map((feature, index) => (
+            <li key={index}>{feature}</li>
+          ))}
+        </ul>
+        <h3>Location</h3>
+        <div className="map-container">
+          <div className="map-canvas" id="map"></div>
+        </div>
+        <h3>Contact Information</h3>
+        <ul>
+          <li>Name: {property.contact.name}</li>
+          <li>Phone: {property.contact.phone}</li>
+          <li>Email: {property.contact.email}</li>
+        </ul>
+        <h3>Additional Information</h3>
+        <p>Asking Price: {property.price}</p>
+        <p>Property History: {property.history}</p>
+        <p>Disclosures: {property.disclosures}</p>
+      </div>
+    </div>
+  );
+};
 
 export default function App() {
   return (
@@ -35,7 +71,7 @@ export default function App() {
             <Home />
           </Route>
         </Switch>
-
+        <Property />
         <Filter />
         <Footer />
       </div>
